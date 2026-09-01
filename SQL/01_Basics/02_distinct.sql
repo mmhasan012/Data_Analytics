890afc99d77506d7bb530151bbb79d55912f180d
Md Mehedi Hasan <131009381+mmhasan012@users.noreply.github.com>
2026-09-01 22:53:18 +0600

    Add DISTINCT examples for departments and cities
    Added examples demonstrating the use of DISTINCT in SQL queries to return unique values for departments and cities.

--- SQL/01_Basics/02_distinct.sql
+++ SQL/01_Basics/02_distinct.sql
@@ -1,1 +1,13 @@
 
+-- SQL Basics: DISTINCT
+-- DISTINCT returns unique values
+
+-- Show all unique departments
+
+SELECT DISTINCT department
+FROM employees;
+
+-- Show all unique cities
+
+SELECT DISTINCT city
+FROM employees;
